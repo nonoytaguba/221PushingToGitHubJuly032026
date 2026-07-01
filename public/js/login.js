4e217df9a4f4680f68c715666766a9b823ad6e5f
Nonoy Taguba <email>
2026-07-01 03:02:51 -0700

    Prepared app for deployment again

--- public/js/login.js
+++ public/js/login.js
@@ -31,7 +31,7 @@ export const logout = async () => {
     try{
         const res = await axios({
             method: 'GET',
-            url: '/api/v1/users/logout',
+            url: '/api/v1/users/logout', //'http://127.0.0.1:3000/api/v1/users/logout', Lesson 222
         });
         if(res.data.status = 'success') location.reload(true);
     }catch(err){
